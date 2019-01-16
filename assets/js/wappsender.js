89a3316665be21f2dc4055a2dd1484664438ef76
frani <franciscoinnocenti@fmbp.local>
2019-01-16 15:14:40 -0300

    ahoy

---
+++ assets/js/wappsender.js
@@ -0,0 +1,25 @@
+var inputs = document.querySelectorAll('input')
+var wapp = 'http://wa.me/5492613453355?text=Hola Nico Vega ✂️ 🛵 : Estoy Buscando un Turno!'
+
+
+function wappear() {
+  for ( let input of inputs){
+    if (input.value !== "") {
+      if (input.name === 'name') {
+        wapp += `, Mi nombre es *${input.value}*`
+      }
+      if (input.name === 'date') {
+        wapp += `, Para el día *${input.value}*`
+      }
+      if (input.name === 'time') {
+        wapp += `, En el horario de las *${input.value}*`
+      }
+      if (input.name === 'message') {
+        wapp += `, info adicional: ${input.value}`
+      }
+    }
+  }
+  wapp += `
+  muchas gracias`
+  window.open(wapp)
+}
